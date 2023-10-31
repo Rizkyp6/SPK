0cc02907ab9f9a8f4892a16dfde6bffbdd8ecefd
Rizkyp6 <149474080+Rizkyp6@users.noreply.github.com>
2023-10-31 06:55:24 -0700

    Add files via upload

---
+++ uts/smartphone_202310311831.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.smartphone (nama,ukuran_layar,memori_internal,kamera_belakang,kapasitas_baterai,harga) VALUES
+	 ('Xiaomi 13T','6.67 inch','256 GB','50MP','5000mAh',6499000.00),
+	 ('vivo Y17s','6.56 inch','128 GB','50MP','5000mAh',1799000.00),
+	 ('realme 11','6.4 inch','256 GB','108MP','5000mAh',3599000.00),
+	 ('Samsung Galaxy z Flip5','3.7 inch','512 GB','12MP','3700mAh',15000000.00),
+	 ('Infinix Hot 30 Play','6.82 inch','64 GB','16MP','6000mAh',1250000.00),
+	 ('Poco F5','6.67 inch','256 GB','64MP','5000mAh',4975000.00),
+	 ('Realme C53 NFC','6.74 inch','128 GB','50MP','5000mAh',1999000.00),
+	 ('Oppo Find N2 Flip','6.8 inch','256 GB','50MP','4300mAh',12150000.00),
+	 ('Vivo V27 5G','6.78 inch','256 GB','50MP','4600mAh',4999000.00),
+	 ('Vivo V27e','6.62 inch','256 GB','64 MP','4600mAh',3550000.00);
